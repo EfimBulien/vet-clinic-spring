2caa49cdc20f370be0b8c6acb26a4ac53ffe3a1b
EfimBulien <isip_e.m.bulien@mpt.ru>
2025-11-17 18:43:25 +0300

    init

---
+++ src/main/java/org/mpt/vet/clinic/VetClinicApplication.java
@@ -0,0 +1,13 @@
+package org.mpt.vet.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mpt/vet/clinic/VetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mpt.vet.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
